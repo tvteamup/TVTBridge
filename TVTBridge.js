154989f3357ff9ed692b5357606e73ab9be78af2
Greg Schoppe <gschoppe@gmail.com>
2025-09-10 22:38:59 -0400

    Update TVTBridge.js

--- TVTBridge.js
+++ TVTBridge.js
@@ -22,12 +22,26 @@ class TVTBridge extends EventTarget {
   constructor(DEBUG=false) {
     super(); // Must call super() to properly initialize the EventTarget.
 
-    this.DEBUG = DEBUG;
+    this.mode = null; /* populated  */
+    this.players = [
+      /*
+      {
+        id: '123456789012234567890',
+        name: 'Player Name',
+        avatarHash: '040103020405002020405065040504',
+        score: 0,
+        data: {}
+      },
+      ...
+      */
+    ];
+    this.playerID = null; /* will eventually either match a player id or 'host' */
+    this.DEBUG = DEBUG; /* boolean */
     if (window.parent === window) {
       throw new Error('TV Team Up apps must be run in a child window.');
     }
 
-    this.initialized = false
+    this.initialized = false;
 
     // Call the method to set up the event listener.
     this._addListeners();
@@ -60,7 +74,6 @@ class TVTBridge extends EventTarget {
    */
   _addListeners() {
     window.addEventListener('message', this._parseDataFromParent.bind(this));
-    this._debuglog(`initialized in ${this.mode} mode.`);
   }
 
   /**
@@ -76,8 +89,9 @@ class TVTBridge extends EventTarget {
     }
 
     const { type, payload } = event.data;
-    let origin = payload.origin || 'unknown';
-    let message = payload.message || '';
+    const origin = payload.origin || 'unknown';
+    const message = payload.message || '';
+    const action = payload.action || {};
 
     if (!type) {
       this._debuglog('Received message with no type.', 'error');
@@ -88,41 +102,49 @@ class TVTBridge extends EventTarget {
     switch (type) {
       case 'INITIALIZE':
         this._initializeGame(payload);
-        this.dispatchEvent(new CustomEvent('gameInitialized', { detail: payload }));
+        this._debuglog(`[${this.mode}]: Received initialization message and initialized game data. Dispatching 'initializeGame' event.`);
+        this.dispatchEvent(new CustomEvent('initializeGame', { detail: payload }));
         break;
       case 'PAUSE':
-        this.dispatchEvent(new Event('gamePaused'));
+        this._debuglog(`[${this.mode}]: Received pause message. Dispatching 'pauseRequested' event.`);
+        this.dispatchEvent(new Event('pauseRequested'));
         break;
       case 'RESUME':
-        this.dispatchEvent(new Event('gameResumed'));
+        this._debuglog(`[${this.mode}]: Received resume message. Dispatching 'resumeRequested' event.`);
+        this.dispatchEvent(new Event('resumeRequested'));
         break;
       case 'MUTE':
-        this.dispatchEvent(new Event('gameMuted'));
+        this._debuglog(`[${this.mode}]: Received mute message. Dispatching 'muteRequested' event.`);
+        this.dispatchEvent(new Event('muteRequested'));
         break;
       case 'UNMUTE':
-        this.dispatchEvent(new Event('gameUnmuted'));
+        this._debuglog(`[${this.mode}]: Received unmute message. Dispatching 'unmuteRequested' event.`);
+        this.dispatchEvent(new Event('unmuteRequested'));
         break;
-      case 'END':
-        this.dispatchEvent(new Event('gameEnded'));
+      case 'GLOBAL_MESSAGE':
+        this._debuglog(`[${this.mode}]: Received global message. Dispatching 'globalMessageReceived' event.`);
+        this.dispatchEvent(new CustomEvent('globalMessageReceived', { detail: {origin, message} }));
+        break;
+      case 'DIRECT_MESSAGE':
+        this._debuglog(`[${this.mode}]: Received direct message. Dispatching 'directMessageReceived' event.`);
+        this.dispatchEvent(new CustomEvent('directMessageReceived', { detail: {origin, message} }));
+        break;
+      case 'PLAYER_ACTION':
+        this._debuglog(`[${this.mode}]: Received player action update. Dispatching 'playerAction' event.`);
+        this.dispatchEvent(new CustomEvent('playerAction', { detail: {origin, action} }));
         break;
       case 'GAME_STATE':
         this._debuglog(`[${this.mode}]: Received game state update. Dispatching 'gameState' event.`);
         this.dispatchEvent(new CustomEvent('gameState', { detail: payload }));
         break;
       case 'PLAYER_STATE':
-        const {playerID, state} = payload;
+        const {playerID, score, data} = payload;
         this._debuglog(`[${this.mode}]: Received player state update. Dispatching 'playerState' event.`);
-        this.dispatchEvent(new CustomEvent('playerState', { detail: {playerID, state} }));
+        this.dispatchEvent(new CustomEvent('playerState', { detail: {playerID, score, data} }));
         break;
-      case 'PLAYER_ACTION':
-        this._debuglog(`[${this.mode}]: Received player action update. Dispatching 'playerAction' event.`);
-        this.dispatchEvent(new CustomEvent('playerAction', { detail: payload }));
-        break;
-      case 'GLOBAL_MESSAGE':
-        this.dispatchEvent(new CustomEvent('globalMessageReceived', { detail: {origin, message} }));
-        break;
-      case 'DIRECT_MESSAGE':
-        this.dispatchEvent(new CustomEvent('directMessageReceived', { detail: {origin, message} }));
+      case 'END':
+        this._debuglog(`[${this.mode}]: Received end message. Dispatching 'endGame' event.`);
+        this.dispatchEvent(new Event('endGame'));
         break;
       default:
         this._debuglog(`[${this.mode}]: Received unknown message type: ${type}`, 'warn');
@@ -168,16 +190,28 @@ class TVTBridge extends EventTarget {
    * @param {string} message The message to send.
    */
   sendGlobalMessage(message) {
-    this._passDataToParent('GLOBAL_MESSAGE', { message, origin: this.playerID });
+    this._passDataToParent('GLOBAL_MESSAGE', { message });
   }
 
   /**
    * Sends a direct message to a specific client via the parent window.
-   * @param {string} targetId The ID of the recipient client.
+   * @param {string} targetID The ID of the recipient client.
    * @param {string} message The message to send.
    */
   sendDirectMessage(targetId, message) {
-    this._passDataToParent('DIRECT_MESSAGE', { message, targetId, origin: this.playerID });
+    this._passDataToParent('DIRECT_MESSAGE', { message, targetID });
+  }
+
+  /**
+   * [CLIENT MODE ONLY] Sends a player action to the parent window, associated with the current playerID.
+   * @param {object} playerAction The action performed by the player (e.g., selected answer).
+   */
+  sendPlayerAction(playerAction) {
+    if (this.mode !== 'client') {
+      this._debuglog('Cannot send player action in host mode.', 'warn');
+      return;
+    }
+    this._passDataToParent('PLAYER_ACTION', playerAction);
   }
 
   /**
@@ -195,33 +229,26 @@ class TVTBridge extends EventTarget {
 
   /**
    * [HOST MODE ONLY] Sends a player state update to the parent window.
-   * @param {string} playerID the id of the player whose states are being updated
+   * @param {string} playerID the id of the player whose states are being updated.
+   * @param {number} score the current player's score.
    * @param {object} state The state of a specific player (e.g., score, health).
    */
-  sendPlayerStateUpdate(playerID, state) {
+  sendPlayerStateUpdate(playerID, score, state) {
     if (this.mode !== 'host') {
       this._debuglog('Cannot send player state update in client mode.', 'warn');
       return;
     }
-    this._passDataToParent('PLAYER_STATE_UPDATE', {playerID, state});
-  }
-
-  /**
-   * [CLIENT MODE ONLY] Sends a player action to the parent window, associated with the current playerID.
-   * @param {object} playerAction The action performed by the player (e.g., selected answer).
-   */
-  sendPlayerAction(playerAction) {
-    if (this.mode !== 'client') {
-      this._debuglog('Cannot send player action in host mode.', 'warn');
-      return;
-    }
-    this._passDataToParent('PLAYER_ACTION', playerAction);
+    this._passDataToParent('PLAYER_STATE_UPDATE', {playerID, score, state});
   }
 
   /**
    * notifies the parent window that the game has ended.
    */
   sendGameEnded() {
-    this._passDataToParent('END');
+    if (this.mode !== 'host') {
+      this._debuglog('Cannot end game in client mode.', 'warn');
+      return;
+    }
+    this._passDataToParent('ENDED');
   }
 }
